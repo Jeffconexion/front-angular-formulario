9253dc8c88ceed36e5289421b210cfceb1009f6f
jeffconexion <jeff.debian2016@gmail.com>
2025-12-24 12:33:07 -0300

    Configuração de rotas.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,6 @@ import { CadastroComponent } from './cadastro/cadastro.component';
 import { ConsultaComponent } from './consulta/consulta.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'cadastro', pathMatch: 'full' },
   { path: 'cadastro', component: CadastroComponent },
   { path: 'consulta', component: ConsultaComponent },
-  { path: '**', redirectTo: 'cadastro' }
 ];
