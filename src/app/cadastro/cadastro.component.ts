fe14c516ee1a0266c17697c857db730cef359165
jeffconexion <jeff.debian2016@gmail.com>
2025-12-30 17:08:13 -0300

    feat: update form layout and styles, add routing links, and include new dependencies

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+
 
 @Component({
   selector: 'app-cadastro',
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule],
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  styleUrls: ['./cadastro.component.scss']
 })
 export class CadastroComponent implements OnInit {
 
